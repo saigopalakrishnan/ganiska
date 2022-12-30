0217a59efccf25a2acca863c7c0eb7058c79b9d1
Gopalakrishnan RAMACHANDRAN <gopalakrishnan.ramachandran@valeo.com>
2022-12-30 16:21:54 +0530

    Ganiska

---
+++ c2-variables.tf
@@ -0,0 +1,11 @@
+# Input Variables
+variable "aws_region" {
+  description = "Region in which AWS Resources to be created"
+  type = string
+  
+}
+
+variable "instance_type" {
+  description = "EC2 Instance Type - Instance Sizing"
+  type = string
+}
